7cf5e0aab1bd8b47fadd8eb0ce31a548bec987a3
Hittkeler <kenhiters@gmail.com>
2019-12-20 12:20:42 -0500

    agregando views

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from VoteSystem.models import  Person, Candidate, RegisterCandidate, SufrageTable, RegisterVote
+admin.site.register(Person)
+admin.site.register(Candidate)
+admin.site.register(RegisterCandidate)
+admin.site.register(SufrageTable)
